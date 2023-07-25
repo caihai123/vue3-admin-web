780e24038a1349c862d578b2873559bf71372db1
caihai <ch15519060282@163.com>
2023-07-25 11:44:43 +0800

    【新增】新增错误页面

---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <Result status="404" title="404" sub-title="Sorry, you are not authorized to access this page.">
+    <template #extra>
+      <Button type="primary">Back Home</Button>
+    </template>
+  </Result>
+</template>
+
+<script setup>
+import { Result, Button } from 'ant-design-vue'
+</script>
---
+++ src/views/500.vue
@@ -0,0 +1,11 @@
+<template>
+  <Result status="500" title="500" sub-title="Sorry, you are not authorized to access this page.">
+    <template #extra>
+      <Button type="primary">Back Home</Button>
+    </template>
+  </Result>
+</template>
+
+<script setup>
+import { Result, Button } from 'ant-design-vue'
+</script>
