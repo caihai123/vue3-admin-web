92029ae36b3d77a17dff594c8b43c9e1b3b40643
caihai <ch15519060282@163.com>
2023-07-31 14:28:59 +0800

    【文档】添加代码注释

--- src/hooks/useInterval/index.js
+++ src/hooks/useInterval/index.js
@@ -3,7 +3,11 @@ import useMemoizedFn from '../useMemoizedFn';
 import { isNumber } from '../utils';
 
 const useInterval = (fn, delay, options = { immediate: false }) => {
+  // 使用useMemoizedFn可保证fn更新时timerCallback的地址永远不会变化
+  // 所以也不需要像delay一样使用watch检测fn的改变后重新注册定时器
+  // 这样的好处在于可以动态修改fn的同时不会打断定时器的进程
   const timerCallback = useMemoizedFn(fn);
+
   const immediate = computed(() => unref(options).immediate);
 
   const timerRef = ref(null);
