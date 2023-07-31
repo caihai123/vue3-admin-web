496c9e4cf4b3484c22cb6e179f5999bcd790bc5f
caihai <ch15519060282@163.com>
2023-07-31 17:22:21 +0800

    【优化】使用useState优化代码

--- src/hooks/useToggle/index.js
+++ src/hooks/useToggle/index.js
@@ -1,15 +1,14 @@
-import { ref } from 'vue';
+import useState from '../useState';
 
 // 默认为 boolean 切换，基础用法与 useBoolean 一致。
 const useToggle = function (leftVal = false, rightVal) {
-  const value = ref(leftVal);
+  const [value, set] = useState(leftVal);
 
   const reverseValueOrigin = rightVal === undefined ? !leftVal : rightVal;
 
-  const toggle = () => (value.value = value.value === leftVal ? reverseValueOrigin : leftVal);
-  const setLeft = () => (value.value = leftVal);
-  const setRight = () => (value.value = rightVal);
-  const set = (val) => (value.value = val);
+  const toggle = () => set(value.value === leftVal ? reverseValueOrigin : leftVal);
+  const setLeft = () => set(leftVal);
+  const setRight = () => set(rightVal);
 
   return [value, { toggle, setLeft, setRight, set }];
 };
