974e46318225873aec5458d7bea3dab82bb088ab
caihai <ch15519060282@163.com>
2023-07-31 14:43:44 +0800

    【更新】导出useMemoizedFn

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,7 @@
 import useBoolean from './useBoolean';
 import useToggle from './useToggle';
+import useMemoizedFn from './useMemoizedFn';
 import useInterval from './useInterval';
 import useTimeout from './useTimeout';
 
-export { useBoolean, useToggle, useInterval, useTimeout };
+export { useBoolean, useToggle, useMemoizedFn, useInterval, useTimeout };
