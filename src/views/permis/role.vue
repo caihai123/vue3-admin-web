6be27dab47de7da0d282621b163dccec58e5106f
caihai <ch15519060282@163.com>
2023-08-03 14:09:05 +0800

    【更新】使用ProTable重构

--- src/views/permis/role.vue
+++ src/views/permis/role.vue
@@ -1,62 +1,29 @@
 <template>
-  <Card style="margin-bottom: 16px" bodyStyle="padding-top: 0px;padding-bottom: 0">
-    <DropdownFrom :model="formState" @finish="(values) => setParams(values)">
-      <Form.Item label="角色名称" name="roleName">
-        <Input v-model:value="formState.roleName" placeholder="请输入角色名称" />
-      </Form.Item>
-      <Form.Item label="状态" name="status">
-        <Input v-model:value="formState.status" placeholder="请选择状态" />
-      </Form.Item>
-    </DropdownFrom>
-  </Card>
-
-  <Card bodyStyle="padding-top: 16px; padding-bottom: 0px">
-    <div class="tools-bar">
-      <div class="header-title">角色列表</div>
-      <div class="tool-right">
-        <Space>
-          <Button type="primary" :icon="h(PlusOutlined)">新增</Button>
-        </Space>
-      </div>
-    </div>
-    <Table
-      rowKey="id"
-      :dataSource="data?.list"
-      :columns="columns"
-      :loading="loading"
-      :pagination="{
-        current: pagination.current,
-        pageSize: pagination.pageSize,
-        total: pagination.total,
-        showSizeChanger: true,
-        showQuickJumper: true,
-        showTotal: (total, range) => `第 ${range.join('-')} 条/共 ${total} 条`,
-        onChange: pagination.onChange,
-      }"
-      bordered
-    >
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'status'">
-          <Switch :checked="record.status === 1" />
-        </template>
-        <template v-else-if="column.key === 'action'">
-          <Space>
-            <Button type="primary" ghost size="small">编辑</Button>
-            <Button type="primary" size="small">授权</Button>
-            <Button type="primary" danger size="small">删除</Button>
-          </Space>
-        </template>
+  <ProTable row-key="id" :columns="columns" :request="request" headerTitle="角色列表">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column?.key === 'status'">
+        <Switch :checked="record.status === 1" />
       </template>
-    </Table>
-  </Card>
+      <template v-else-if="column?.key === 'action'">
+        <Space>
+          <Button type="primary" ghost size="small">编辑</Button>
+          <Button type="primary" size="small">授权</Button>
+          <Button type="primary" danger size="small">删除</Button>
+        </Space>
+      </template>
+    </template>
+
+    <template #toolBar>
+      <Button type="primary" :icon="h(PlusOutlined)">新增</Button>
+    </template>
+  </ProTable>
 </template>
 
 <script setup>
-import { reactive, h } from 'vue';
-import { Card, Table, Space, Button, Form, Input, Switch } from 'ant-design-vue';
-import DropdownFrom from '@/components/DropdownFrom.vue';
-import { usePagination, useState } from '@/hooks/index';
 import axios from '@/utils/axios';
+import { h } from 'vue';
+import ProTable from '@/components/ProTable/index.vue';
+import { Switch, Space, Button } from 'ant-design-vue';
 import { PlusOutlined } from '@ant-design/icons-vue';
 
 const columns = [
@@ -67,40 +34,34 @@ const columns = [
   {
     title: '备注',
     dataIndex: 'description',
+    hideInSearch: true,
   },
   {
     title: '状态',
     key: 'status',
+    type: 'select',
+    options: [
+      { label: '启用', value: '1' },
+      { label: '禁用', value: '0' },
+    ],
   },
   {
     title: '操作',
     key: 'action',
     width: 100,
     fixed: 'right',
+    hideInSearch: true,
   },
 ];
-const formState = reactive({
-  title: '',
-  path: '',
-});
-const [params, setParams] = useState({});
 
-const { data, pagination, loading } = usePagination(
-  ({ current, pageSize }) => {
-    return axios
-      .post('/api/role/page', { params: params.value, pageIndex: current, pageSize })
-      .then((value) => {
-        const { result: data } = value;
-        return {
-          list: data.records || [],
-          total: data.total,
-        };
-      });
-  },
-  {
-    refreshDeps: [params],
-  }
-);
+const request = (params, { current, pageSize }) =>
+  axios.post('/api/role/page', { params, pageIndex: current, pageSize }).then((value) => {
+    const { result: data } = value;
+    return {
+      list: data.records,
+      total: data.total,
+    };
+  });
 </script>
 
 <style scoped>
